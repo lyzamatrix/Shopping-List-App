2dcdea8f51abe63559f373b08ba72abe259f843c
Lyza <lyzamatrix.github.com>
2018-07-26 13:49:14 +0800

    initial commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDFR3lekiKVpFLja04hvourUuIh_estTTE",
+  authDomain: "shopping-list-22bac.firebaseapp.com",
+  databaseURL: "https://shopping-list-22bac.firebaseio.com",
+  projectId: "shopping-list-22bac",
+  storageBucket: "",
+  messagingSenderId: "822138416370"
+};
